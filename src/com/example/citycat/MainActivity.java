591e906ee5d7171fdc4239c92d25503034e1948e
shirly shenk <shirlyshenk@gmail.com>
2013-01-31 11:31:22 +0200

    checking
    check

--- src/com/example/citycat/MainActivity.java
+++ src/com/example/citycat/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends Activity {
 	TextView hotEventsText;
 	Scroller hotEventsScroller;
 	Context context;
-	
+	/////////
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
