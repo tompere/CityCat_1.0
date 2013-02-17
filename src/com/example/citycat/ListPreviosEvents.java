9ffba03958d9f7bacb32c84511348c3a4b8c031f
shirly shenk <shirlyshenk@gmail.com>
2013-02-17 19:28:14 +0200

    Adding previos events of the user
    the activity presents a list of the previos events of the user

--- src/com/example/citycat/ListPreviosEvents.java
+++ src/com/example/citycat/ListPreviosEvents.java
@@ -1,11 +1,12 @@
 package com.example.citycat;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import android.app.Activity;
-import android.app.ActivityGroup;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -19,7 +20,7 @@ import com.parse.ParseObject;
 import com.parse.ParseQuery;
 
 
-public class ListEvent extends Activity {
+public class ListPreviosEvents extends Activity {
 	ListView list;
 	ArrayList<String> ListEventNames;
 	Context thisContext;
@@ -28,15 +29,22 @@ public class ListEvent extends Activity {
 	
 	
 	protected void onCreate(Bundle savedInstanceState) {
+		
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_list_event);
-		list=(ListView) findViewById(R.id.list_events);
+		setContentView(R.layout.activity_previos_events);
+		Log.d("check", "check1" );
+		list=(ListView) findViewById(R.id.list_previos_events);
 		ListEvents=new ArrayList<String>();
 		Parse.initialize(this, "sN3Uhl2rCCJvp1rodg9hYqw9pZN8kVkYuPCCwn5D", "ECprIUSxorEFhSSzq7ani1dR7Up4gApnjAmPFjiY");
-
-		
+		Log.d("check", "check2" );
+		 SharedPreferences ref = getSharedPreferences("user_details",MODE_PRIVATE);
+		 Log.d("check", "check3" );
+		String user_name=ref.getString("username","unknown");
+		Log.d("publisher", "publisher is" + user_name );
 	
 		ParseQuery query = new ParseQuery("Event");
+		query.whereEqualTo("publisher", user_name);
+		
 		query.findInBackground(new FindCallback() {
 			public void done(List<ParseObject> objects, ParseException e) {
 			
@@ -47,9 +55,13 @@ public class ListEvent extends Activity {
 					for (i=0; i<objects.size(); i++) 
 					{
 						ParseObject ParseEvent = objects.get(i);
-						name_event= ParseEvent.getString("name").toString() +"  "+ParseEvent.getString("date").toString();;
+						Date date = ParseEvent.getDate("date");
+						String dateFormat = date.getDay() + 
+								"/" + date.getMonth() + 
+								"/" + date.getYear();
+						name_event= ParseEvent.getString("name").toString() + " | " + dateFormat;
 						
-						Log.d("check",name_event);
+						Log.d("check name ",name_event);
 						ListEvents.add(name_event);
 						
 					}
@@ -59,7 +71,7 @@ public class ListEvent extends Activity {
 				}
 				else
 				{
-					
+					Log.d("ddddaaaa", "ssssaaaaa");
 				}
 			
 		
--- src/com/example/citycat/MainActivity.java
+++ src/com/example/citycat/MainActivity.java
@@ -3,6 +3,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.animation.LinearInterpolator;
@@ -11,43 +12,49 @@ import android.widget.Scroller;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-	
+
 	Button postEvent;
 	Button  ChooseEvent;
 	Intent goToPostEvent;
 	Intent goToChooseEvent;
+	Intent goToPreviousEvents;
 	TextView hotEventsText;
+	TextView PreviosEvents;
+
 	Scroller hotEventsScroller;
 	Context context;
-	
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        hotEventsText = (TextView)this.findViewById(R.id.hotEvents);
-        context = this;
-        
-        postEvent = (Button)this.findViewById(R.id.location);
-        ChooseEvent=(Button)this.findViewById(R.id.ChooseEvent);
-        goToPostEvent = new Intent(this, ChooseLocation.class);   
-        goToChooseEvent= new Intent(this, TabsEvents.class);
-     
-        clickHandler click = new clickHandler();
-        postEvent.setOnClickListener(click);
-        ChooseEvent.setOnClickListener(click);
-        
-        Thread hotEventsThread = new Thread(hotEvents);
-        hotEventsThread.start();
-      
-        
-    	
- 
-    }
 
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
-    
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		hotEventsText = (TextView)this.findViewById(R.id.hotEvents);
+		context = this;
+
+		PreviosEvents=(Button)this.findViewById(R.id.YourEvent);
+		postEvent = (Button)this.findViewById(R.id.location);
+		ChooseEvent=(Button)this.findViewById(R.id.ChooseEvent);
+		goToPostEvent = new Intent(this, ChooseLocation.class);   
+		goToChooseEvent= new Intent(this, TabsEvents.class);
+		goToPreviousEvents= new Intent(this, ListPreviosEvents.class);
+
+
+		clickHandler click = new clickHandler();
+		postEvent.setOnClickListener(click);
+		ChooseEvent.setOnClickListener(click);
+		PreviosEvents.setOnClickListener(click);
+		Thread hotEventsThread = new Thread(hotEvents);
+		hotEventsThread.start();
+
+
+
+
+	}
+
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
 	class clickHandler implements View.OnClickListener {
 		public void onClick(View v)
 		{
@@ -55,28 +62,37 @@ public class MainActivity extends Activity {
 			{
 				startActivity(goToPostEvent);
 			}
-			
+
 			if ((Button)v == ChooseEvent)
-				
+
 			{
+				Log.d("checkingMain","check333");
 				startActivity(goToChooseEvent);
+			}
+
+			if ((Button)v == PreviosEvents)
+
+			{
+				
+				startActivity(goToPreviousEvents);
+				
 			}
 		}
 	}
-	
+
 	private Runnable hotEvents = new Runnable() {
-    	public void run() { 		
-    		hotEventsScroller = new Scroller(context, new LinearInterpolator());
-    		hotEventsText.setText("It's true that all the men you knew" 
-    								+ "\n" + "were dealers who said they were through"
-    								+ "\n" + "with dealing Every time you gave them shelter");
-    		hotEventsText.setScroller(hotEventsScroller);	
-    		while (true)
-    		{
-    			if (hotEventsScroller.isFinished())
-    				hotEventsScroller.startScroll(0, 0, 0, 500, 5000);
-    		}
-    	}
-    };
-	
+		public void run() { 		
+			hotEventsScroller = new Scroller(context, new LinearInterpolator());
+			hotEventsText.setText("It's true that all the men you knew" 
+					+ "\n" + "were dealers who said they were through"
+					+ "\n" + "with dealing Every time you gave them shelter");
+			hotEventsText.setScroller(hotEventsScroller);	
+			while (true)
+			{
+				if (hotEventsScroller.isFinished())
+					hotEventsScroller.startScroll(0, 0, 0, 500, 5000);
+			}
+		}
+	};
+
 }
